32ff223daff9d2a896f19438d85546fc84e3998f
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-02-12 17:51:25 +0900

    [Init] Gradle Project in IntelliJ IDEA

---
+++ src/main/java/com/masahirosaito/spigot/anticreeper/AntiCreeper.kt
@@ -0,0 +1,13 @@
+package com.masahirosaito.spigot.anticreeper
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class AntiCreeper : JavaPlugin() {
+
+    override fun onEnable() {
+
+    }
+
+    override fun onDisable() {
+    }
+}
